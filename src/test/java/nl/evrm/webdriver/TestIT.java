7e98528487c898b07341ade4eb58f4dba524ab01
Erik-jan Vermaire <evrm@axi.nl>
2016-11-02 15:50:20 +0100

    Added import for ChromeOptions

--- src/test/java/nl/evrm/webdriver/TestIT.java
+++ src/test/java/nl/evrm/webdriver/TestIT.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import java.io.File;
